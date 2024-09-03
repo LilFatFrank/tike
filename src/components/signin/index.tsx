120bbc6857d98e084bfb6d76f10b6ea4250da401
LilFatFrank <karan.parsnani@ymail.com>
2024-09-03 20:57:52 +0530

    updating styling

--- src/components/signin/index.tsx
+++ src/components/signin/index.tsx
@@ -16,20 +16,27 @@ const SignIn: FC = () => {
           alt="signin"
           className="fixed z-[-1] object-cover w-full h-full md:w-[550px] md:rounded-t-[20px] md:h-[calc(100dvh-40px)]"
         />
-        <p className="font-satisfy font-normal text-white text-[40px] leading-[120%] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <p className="font-satisfy font-normal text-white text-[64px] leading-[120%] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
           Tike
         </p>
         <div className="flex flex-col items-left justify-center w-[90%] gap-4">
-          <p className="font-extrabold text-[40px] leading-[48px] text-white">
+          <p className="font-extrabold text-[40px] leading-[48px] text-white/10">
             Your hub for fun, creativity, and community.
           </p>
           <div className="relative z-20 flex w-full cursor-pointer items-center overflow-hidden rounded-[100px] mb-[40px]">
             <div className="animate-rotate absolute inset-0 h-full w-full rounded-full bg-[conic-gradient(white_20deg,transparent_120deg)]" />
-            <div className="absolute inset-[1px] rounded-[99px] bg-sign-in-bg" />
+            <div className="absolute inset-[1px] rounded-[100px] bg-sign-in-bg" />
             <div className="relative z-20 w-full bg-auth-btn-bg rounded-[100px]">
               <NeynarAuthButton
                 variant={SIWN_variant.FARCASTER}
-                className="login-btn backdrop-blur-(10px) font-grotesk z-20"
+                icon={
+                  <img
+                    src="/icons/farcaster-sign-in-icon.svg"
+                    alt="sign-in-farcaster"
+                    className="w-6 h-6"
+                  />
+                }
+                className="login-btn backdrop-blur-(10px) rounded-[100px] font-grotesk z-20"
               />
             </div>
           </div>
