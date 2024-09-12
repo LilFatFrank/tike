678f595e110d6561279d9fabab0903bb09a4c64d
LilFatFrank <karan.parsnani@ymail.com>
2024-09-13 02:13:03 +0530

    optimizing for pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,12 +43,27 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <meta name="theme-color" content="#472B92" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
         <link rel="apple-touch-icon" href="/images/192x192-logo.png" />
         <link rel="icon" href="/logo/favicon.svg" type="image/svg+xml" />
         {process.env.NEXT_PUBLIC_GTM_ID ? (
           <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
         ) : null}
+      </head>
+      <body className={`${inter.className}`}>
+        <Toaster position="bottom-center" duration={1500} />
+        <main className="w-dvw max-md:h-dvh md:h-[calc(100dvh-40px)] mx-auto md:w-[552px] md:border md:border-black-20 md:border-b-0 md:rounded-t-[20px] md:overflow-auto md:no-scrollbar">
+          <Wrapper>{children}</Wrapper>
+        </main>
         <Script
           id="register-sw"
           strategy="afterInteractive"
@@ -56,7 +71,7 @@ export default function RootLayout({
             __html: `
               if ('serviceWorker' in navigator) {
                 window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js').then(
+                  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(
                     function(registration) {
                       console.log('Service Worker registered with scope:', registration.scope);
                     },
@@ -69,12 +84,6 @@ export default function RootLayout({
             `,
           }}
         />
-      </head>
-      <body className={`${inter.className}`}>
-        <Toaster position="bottom-center" duration={1500} />
-        <main className="w-dvw max-md:h-dvh md:h-[calc(100dvh-40px)] mx-auto md:w-[552px] md:border md:border-black-20 md:border-b-0 md:rounded-t-[20px] md:overflow-auto md:no-scrollbar">
-          <Wrapper>{children}</Wrapper>
-        </main>
       </body>
     </html>
   );
