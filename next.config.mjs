7ca6ed12a6029bee87e69b8c35f126b00b591cf0
LilFatFrank <karan.parsnani@ymail.com>
2024-09-13 14:12:01 +0530

    removing pwa implementation

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withPWA from "next-pwa";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -18,67 +16,4 @@ const nextConfig = {
   swcMinify: true,
 };
 
-const pwaConfig = withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "google-fonts",
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60,
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-font-assets",
-      },
-    },
-    {
-      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-image-assets",
-      },
-    },
-    {
-      urlPattern: /\.(?:js)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-js-assets",
-      },
-    },
-    {
-      urlPattern: /\.(?:css|less)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-style-assets",
-      },
-    },
-    {
-      urlPattern: /\.(?:json|xml|csv)$/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "static-data-assets",
-      },
-    },
-    {
-      urlPattern: /.*/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "others",
-        networkTimeoutSeconds: 10,
-      },
-    },
-  ],
-});
-
-export default pwaConfig(nextConfig);
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { Wrapper } from "@/components";
 import { Toaster } from "sonner";
 import { GoogleTagManager } from "@/components";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,49 +40,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
-        <meta name="theme-color" content="#472B92" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-        <link rel="apple-touch-icon" href="/images/192x192-logo.png" />
-        <link rel="icon" href="/logo/favicon.svg" type="image/svg+xml" />
-        {process.env.NEXT_PUBLIC_GTM_ID ? (
+      <body className={inter.className}>
+        {process.env.NEXT_PUBLIC_GTM_ID && (
           <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
-        ) : null}
-      </head>
-      <body className={`${inter.className}`}>
+        )}
         <Toaster position="bottom-center" duration={1500} />
         <main className="w-dvw max-md:h-dvh md:h-[calc(100dvh-40px)] mx-auto md:w-[552px] md:border md:border-black-20 md:border-b-0 md:rounded-t-[20px] md:overflow-auto md:no-scrollbar">
           <Wrapper>{children}</Wrapper>
         </main>
-        <Script
-          id="register-sw"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              if ('serviceWorker' in navigator) {
-                window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(
-                    function(registration) {
-                      console.log('Service Worker registered with scope:', registration.scope);
-                    },
-                    function(err) {
-                      console.error('Service Worker registration failed:', err);
-                    }
-                  );
-                });
-              }
-            `,
-          }}
-        />
       </body>
     </html>
   );
